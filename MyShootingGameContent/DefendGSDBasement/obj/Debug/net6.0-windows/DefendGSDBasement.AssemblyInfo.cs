f7769a611b5c6b252c09300cc93b8422bde65ddf
ILaughU3000 <78650714+longlongknight@users.noreply.github.com>
2023-12-13 04:33:28 +0800

    exe

--- MyShootingGameContent/DefendGSDBasement/obj/Debug/net6.0-windows/DefendGSDBasement.AssemblyInfo.cs
+++ MyShootingGameContent/DefendGSDBasement/obj/Debug/net6.0-windows/DefendGSDBasement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefendGSDBasement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d399d80ec6311ff6da62771256cefb24b91cd068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefendGSDBasement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefendGSDBasement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
